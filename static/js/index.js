a39184d30bdf1b9003265c456e930a37c99a1003
shal-in <shalin.a.ahasan@gmail.com>
2023-07-12 22:06:32 +0100

    fully working two way connection on home page

--- static/js/index.js
+++ static/js/index.js
@@ -1,84 +1,139 @@
+const socket = io();
+let sessionid;
+let gameid;
+
 const titleBtnEl = document.getElementById('title-btn');
-
 const startBtns = document.querySelectorAll('.start-btn');
-const localBtnEl = startBtns[0]
-const createBtnEl = startBtns[1]
-const joinBtnEl = startBtns[2]
-
+const localBtnEl = startBtns[0];
+const createBtnEl = startBtns[1];
+const joinBtnEl = startBtns[2];
 const btnContainerEl = document.getElementById('btn-container');
-const inputContainerEl = document.getElementById('input-container'); 
+const inputContainerEl = document.getElementById('input-container');
+const gameInputEl = document.getElementById('game-input');
+const playBtnEl = document.getElementById('play-btn');
 
-const gameInputEl = document.createElement('input');
-const playBtnEl = document.createElement('button');
 
 titleBtnEl.addEventListener('click', function titleBtnFunction() {
-    window.open('https://github.com/shal-in/', "_blank");
+    window.open('https://github.com/shal-in/', '_blank');
 });
-
 localBtnEl.addEventListener('click', localBtnFunction);
-
 createBtnEl.addEventListener('click', createBtnFunction);
-
 joinBtnEl.addEventListener('click', joinBtnFunction);
+playBtnEl.addEventListener('click', () => playBtnFunction(gameInputEl));   
+gameInputEl.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        playBtnFunction(gameInputEl);
+    }
+});
 
 function localBtnFunction() {
     console.log('local');
+
+    payLoad = {
+        key: 'value'
+    }
+    socket.emit('local', payLoad);
 }
 
 function createBtnFunction() {
-    console.log('create')
+    console.log('create');
+
+    socket.emit('create');
 }
 
 function joinBtnFunction() {
-    event.stopPropagation()
+    event.stopPropagation();
 
     startBtns.forEach((btn) => {
         btn.style.backgroundColor = 'blue';
     });
 
-    gameInputEl.id = 'game-input';
-    gameInputEl.placeholder = 'add game ID';
-
-    playBtnEl.id = 'play-btn';
-    playBtnEl.textContent = 'play';
-    playBtnEl.addEventListener('click', () => playBtnFunction(gameInputEl));
-    
-    gameInputEl.addEventListener('keyup', (event) => {
-        if (event.keyCode === 13) {
-            playBtnFunction(gameInputEl);
-        }
-    });
-
-    inputContainerEl.appendChild(gameInputEl);
-    inputContainerEl.appendChild(playBtnEl);
+    inputContainerEl.style.display = 'flex'
 
     localBtnEl.removeEventListener('click', localBtnFunction);
     createBtnEl.removeEventListener('click', createBtnFunction);
     joinBtnEl.removeEventListener('click', joinBtnFunction);
-    
-    btnContainerEl.addEventListener('click', btnContainerFunction)
+    btnContainerEl.addEventListener('click', btnContainerFunction);
+
+    gameInputEl.focus()
 }
 
 function btnContainerFunction() {
-    console.log('test')
-    gameInputEl.remove();
-    playBtnEl.remove();
-
-    localBtnEl.addEventListener('click', localBtnFunction);
-    createBtnEl.addEventListener('click', createBtnFunction);
-    joinBtnEl.addEventListener('click', joinBtnFunction);
-
     startBtns.forEach((btn) => {
         btn.style.backgroundColor = 'red';
     });
 
-    btnContainerEl.removeEventListener('click', btnContainerFunction)
+    inputContainerEl.style.display = 'none';
+
+    localBtnEl.addEventListener('click', localBtnFunction);
+    createBtnEl.addEventListener('click', createBtnFunction);
+    joinBtnEl.addEventListener('click', joinBtnFunction);
+    btnContainerEl.removeEventListener('click', btnContainerFunction);
 }
 
 function playBtnFunction(inputEl) {
     console.log('play');
-    let gameInputValue = inputEl.value;
-    if (gameInputValue) {
-        console.log(gameInputValue);
+    gameid = inputEl.value;
+    if (gameid) {
+        inputEl.value = '';
+        if (gameid.length !== 11) {
+            alert('invalid gameid');
+            return;
+        } 
+
+        payLoad = {
+            'gameid': gameid
+        };
+
+        socket.emit('join', payLoad);
     }
 }
+
+socket.on('connection_response', function(data) {
+    sessionid = data.sessionid;
+    console.log(`sessionid: ${sessionid}`);
+});
+
+socket.on('create_response', function(data) {
+    gameid = data.gameid;
+    console.log(`gameid: ${gameid}`);
+    copyToClipboard(gameid);
+});
+
+socket.on('join_error', function(data) {
+    alert(data.message);
+    gameid;
+})
+
+socket.on('join_response', function(data) {
+    console.log(`${gameid} joined successfully`);
+    console.log(data.message)
+})
+
+
+
+
+
+
+
+
+
+
+function copyToClipboard(text) {
+    if (navigator.clipboard && window.isSecureContext) {
+      navigator.clipboard.writeText(text)
+        .catch((error) => {
+          console.error("Failed to copy text: ", error);
+        });
+    } else {
+      const textarea = document.createElement('textarea');
+      textarea.value = text;
+      document.body.appendChild(textarea);
+  
+      textarea.select();
+      document.execCommand('copy');
+  
+      document.body.removeChild(textarea);
+    }
+  }
+  
